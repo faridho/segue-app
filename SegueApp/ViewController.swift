11a08afac480ba4ae65e7e617d3b104d9e21e38c
Faridho <faridho@zeepos.com>
2020-03-11 21:48:03 +0700

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Faridho on 11/03/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
